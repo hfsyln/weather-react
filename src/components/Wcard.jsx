45786deb4a29b5c42a74b380b6e786f0f01e2873
hafsa <hfsyln93@gmail.com>
2022-10-27 00:23:18 +0300

    router

--- src/components/Wcard.jsx
+++ src/components/Wcard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 
+
 const Wcard = ({setWeather,  weather}) => {
   console.log(weather)
+
+
+    
+  console.log(weather)
+  if (!weather.weather) {
+      return <h1>Loading</h1>
+  }
+
     return (
     <>
     <Card style={{ width: '20rem' }} className= "m-3" key={weather?.id}>
@@ -10,7 +19,7 @@ const Wcard = ({setWeather,  weather}) => {
             <Card.Title className="mr-0">{weather?.name}</Card.Title>
             <Card.Subtitle className="text-muted my-5">
                 <h2 className="text-center">{Math.floor(weather?.temp/33.8)}°C</h2>
-                <p className="text-center"></p>
+                <p className="text-center">{weather?.weather[0].description}</p>
             </Card.Subtitle>
             <Card.Text className="text-muted  ">
                 <Card.Text>
