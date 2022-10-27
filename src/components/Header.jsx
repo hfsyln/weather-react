ad4688b89b1a82c3d5b9f573e9b9749daa67fe22
hafsa <hfsyln93@gmail.com>
2022-10-27 20:28:56 +0300

    edit css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,23 +7,20 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-   <>
-    <Navbar className=" d-flex justify-content-start " bg="light" variant="light">
-        <Container className=" d-flex justify-content-start mx-0">
-        <Nav className=" d-flex justify-content-start gap-5 " >
+   <div className='navbar'>
+    <Navbar >
+        <Container className=" d-flex justify-content-center ">
+            <Nav className=" d-flex justify-content-start gap-5" >
                 <Link className='text-decoration-none text-dark'  to="/weatherPage">Weather App</Link>  
                 <Link className='text-decoration-none text-dark' to="/">Home</Link> 
                 <Link className='text-decoration-none text-dark' to="/turkey-map">Turkey cities map</Link>  
-                <Link className='text-decoration-none text-dark' to="/turkey-cities">List of cities turkey</Link>  
+                <Link className='text-decoration-none text-dark' to="/turkey-cities">List of cities Turkey</Link>  
             </Nav>
         </Container>
     </Navbar>
-    <div className="main ">
-            <h1 className="title d-flex justify-content-center">Welcome</h1>
-            <p className="title d-flex justify-content-center">Please Click Button For Other Countries' Weather Condition</p>
-    </div>
    
-</>
+   
+</div>
   )
 }
 
